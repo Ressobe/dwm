0a353a63e3ac9a7ae6cc457bb85b9f09c9cec699
Bartosz Sobina <bartosz.sobina44@gmail.com>
2021-08-11 11:22:00 +0200

    autostart script

---
+++ autostart.sh
@@ -0,0 +1,5 @@
+xrandr --output HDMI-0 --mode 1920x1080 --rate 144.01 &
+xrandr --output DP-3 --mode 1920x1080 --right-of HDMI-0 &
+feh --bg-scale /home/relow/.config/wallpapers/arch.png &
+slstatus &
+picom &
