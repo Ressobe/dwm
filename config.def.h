dad058a8d4e639b25b58ebed0bf7eb0c5146952f
Bartosz Sobina <bartosz.sobina@tutanota.com>
2021-11-21 20:47:47 +0100

    Version 2.0

--- config.def.h
+++ config.def.h
@@ -1,23 +1,23 @@
 #include "/home/relow/.cache/wal/colors-wal-dwm.h"
 #include "movestack.c"
 
-static const unsigned int borderpx  = 0;        // border pixel of windows 
+static const unsigned int borderpx  = 1;        // border pixel of windows 
 static const unsigned int snap      = 32;       // snap pixel 
-static const unsigned int gappih    = 12;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 12;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 12;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
-static int smartgaps          		= 0;        /* 1 means no outer gap when there is only one window */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static int smartgaps                = 0;        /* 1 means no outer gap when there is only one window */
 static const int swallowfloating    = 0;        // 1 means swallow floating windows by default 
 static const int showbar            = 1;        // 0 means no bar 
 static const int topbar             = 1;        // 0 means bottom bar 
-static const char *fonts[]          = { "firacode:size=11", "fontawesome:size=14" };
+static const char *fonts[]          = { "hacknerdfont:size=12", "fontawesome:size=14" };
 
-static const char *tags[] = { "code", "gui", "web", "files","music"  };
+static const char *tags[] = { " code", " gui", " web", " music"  };
 static const Rule rules[] = {
 	// class     		instance   		title          tags mask  	isfloating  isterminal  noswallow  monitor 
-	{ "Alacritty",	    NULL,     		NULL,          0,         	0,          1,           0,        -1 },
-	{ "Brave",       	NULL,     		NULL,          1 << 2,      0,          0,			 0,        -1 },
+	{ "Alacritty",	    NULL,     		NULL,          0,           0,          1,           0,        -1 },
+	{ "Brave",          NULL,     		NULL,          1 << 2,      0,          0,	     0,        -1 },
 };
 
 static const float mfact     				= 0.55; /* factor of master area size [0.05..0.95] */
@@ -69,27 +69,29 @@ static const char *power[] 		  =   {"r-power.sh", NULL};
 static const char *browser[] 	  =   {"brave", NULL};
 static const char *audio[] 		  =   {"pavucontrol", NULL};
 static const char *wallpaper[]    =   {"r-wallpaper.sh", NULL};
-static const char *doc[]          =   {"r-doc.sh",NULL};
 static const char *man[]          =   {"r-man.sh",NULL};
 static const char *lightup[]      =   {"sudo","xbacklight", "-inc", "5" , NULL};
 static const char *lightdown[]    =   {"sudo","xbacklight", "-dec", "5" , NULL};
+static const char *volumeup[]     =   {"sudo", "amixer set Master 10%+", NULL};
+static const char *volumedown[]   =   {"sudo", "amixer set Master 10%-", NULL};
 static const char *bluetooth[]    =   {"blueberry", NULL};
 static const char *killprocess[]  =   {"r-kill.sh", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      	spawn,         {.v = dmenucmd } },
-	{ MODKEY,             			XK_Return, 	spawn,         {.v = termcmd } },
-	{ ALTMOD,						XK_b,	   	spawn, 		   {.v = browser } },
-	{ ALTMOD,						XK_1,	  	spawn, 		   {.v = doc } },
-	{ ALTMOD,						XK_2,	  	spawn, 		   {.v = man } },
-	{ ALTMOD,						XK_3,	  	spawn, 		   {.v = wallpaper } },
-	{ ALTMOD,                       XK_4,     	spawn,         {.v = power } },
-	{ ALTMOD,                       XK_5,     	spawn,         {.v = audio } },
-	{ ALTMOD,                       XK_6,     	spawn,         {.v = bluetooth } },
-	{ ALTMOD,                       XK_9,     	spawn,         {.v = lightdown } },
-	{ ALTMOD,                       XK_0,     	spawn,         {.v = lightup } },
-	{ ALTMOD|ShiftMask,             XK_c,     	spawn,         {.v = killprocess } },
+	{ MODKEY,                       XK_d,      spawn,         {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return, spawn,         {.v = termcmd } },
+	{ ALTMOD,                       XK_b,      spawn,    {.v = browser } },
+	{ ALTMOD,                       XK_2,      spawn,    {.v = man } },
+	{ ALTMOD,                       XK_3,      spawn,    {.v = wallpaper } },
+	{ ALTMOD,                       XK_4,     spawn,         {.v = power } },
+	{ ALTMOD,                       XK_5,     spawn,         {.v = audio } },
+	{ ALTMOD,                       XK_6,     spawn,         {.v = bluetooth } },
+	{ ALTMOD,                       XK_9,     spawn,         {.v = lightdown } },
+	{ ALTMOD,                       XK_0,     spawn,         {.v = lightup } },
+	{ ALTMOD|ShiftMask,             XK_c,     spawn,         {.v = killprocess } },
+	{ ALTMOD|ShiftMask,             XK_9,     spawn,         {.v = volumedown } },
+	{ ALTMOD|ShiftMask,             XK_0,     spawn,         {.v = volumeup } },
 	// WINDOW MANIPULATION
 	{ MODKEY,            	        XK_b,      togglebar,      {0} },
 	{ MODKEY,             			XK_q,      killclient,     {0} },
@@ -128,7 +130,6 @@ static Key keys[] = {
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
 	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
 	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
 };
 
